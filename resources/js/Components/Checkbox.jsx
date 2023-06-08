714e7ad7ef52f598a0b2271b5d4a3e724ee38d88
unknown <taqiussa>
2023-06-08 15:11:35 +0700

    checkbox

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -1,8 +1,9 @@
-export default function Checkbox({ className = '', ...props }) {
+export default function Checkbox({ className = '', handleChange, ...props }) {
     return (
         <input
             {...props}
             type="checkbox"
+            onChange={(e) => handleChange(e)}
             className={
                 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 ' +
                 className
